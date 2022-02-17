4c6faf084d97bbd17b3cb0455814ff19a2808ebc
Hulikner <hulikner@gmail.com>
2022-02-17 11:35:55 -0600

    First code

---
+++ main.js
@@ -0,0 +1,16 @@
+
+// const printIntroduction = () => {
+//     const name = 'Derick';
+//     const food = 'pho';
+//     console.log(`My name is ${name}, and my favorite food is ${food}.`);
+// }
+
+// printIntroduction();
+
+function printIntroduction(name, food){
+    // const name = 'Derick';
+    // const food = 'pho';
+    console.log('My name is ' + name + ', and my favorite food is ' + food + '.');
+}
+
+printIntroduction('Derick','pho');
